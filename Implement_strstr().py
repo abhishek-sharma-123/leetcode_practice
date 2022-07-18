be9db7939944fc1ca42efa15e6523041e03e4eb4
Abhiiii02 <109423843+Abhiiii02@users.noreply.github.com>
2022-07-18 16:05:02 +0530

    Updated description

--- Implement_strstr().py
+++ Implement_strstr().py
@@ -1,3 +1,8 @@
+#Implement strStr().
+
+#Given two strings needle and haystack, 
+#return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle in haystack :
