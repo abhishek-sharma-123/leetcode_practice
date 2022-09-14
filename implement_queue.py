0849020c49f426d7889688ea28e341bb3b2d97b2
Abhiiii02 <109423843+Abhiiii02@users.noreply.github.com>
2022-09-15 03:17:02 +0530

    Implement queue
    Implement queue using stack

---
+++ implement_queue.py
@@ -0,0 +1,19 @@
+class MyQueue:
+
+    def __init__(self):
+        self.queue=[]
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        n=self.queue.pop(0)
+        return n 
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        if self.queue==[]:
+            return True
+        return False
